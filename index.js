c39b7a28e8dc6d30ce99f7fce6359ef57e4bee21
retrohacker <william.jblankenship@gmail.com>
2016-10-24 22:13:27 -0400

    Comment index.js

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+// Receiver is responsible for registering an SMTP client that receives emails
+// destined for [UUID]@heroku.storj.io
 var Receiver = require('./lib/receiver');
+// Heroku is used to resolve the UUID of an incomming email to an actual heroku
+// user, fetching that user's email address in case we need to forward it.
 var Heroku = require('./lib/heroku');
+// In the event we forward an email, Mailer is used.
 var Mailer = require('./lib/sender');
+// Load in our runtime configuration.
 var config = require('./config');
+// MailParser is used to handle parsing an SMTP message out into usable
+// components such as the body, subject, and sender.
 var MailParser = require('mailparser').MailParser;
+// Receiver stores a the incomming SMTP message on the filesystem, we need to
+// read it from disk so we use fs.
 var fs = require('fs');
+// autoaccept contains the logic for automatically accepting an incomming
+// registration email for a heroku user.
 var autoaccept = require('./lib/autoaccept');
+// We use bole for logging, it is configured in ./config
 var log = require('bole')('account-mapper')
 
+// onEmail is the event handler that is triggered when a new email is received
+// by the server.
 function onEmail(address, pathname, cb) {
   log.info('Received email for %s', address.address);
-  this.heroku.getEmail(address.local, function haveHerokuEmail(e, forwardAddr) {
+  // Resolve the UUID for incomming email to a heroku account. This also
+  // verifies that the incoming email belongs to a real heroku add-on, and
+  // that this API isn't being abused to register bot accounts. If the UUID
+  // does not exist on Heroku's end, we will return an error instead of
+  // registering the account.
+  this.heroku.getEmail(address.local, function haveEmail (e, forwardAddr) {
+    // If we encounter an error, then we should assume the heroku address does
+    // not exist, and we should return.
     if(e) {
       log.error(e);
       return cb(e);
@@ -17,8 +39,26 @@ function onEmail(address, pathname, cb) {
 
     log.info('Successfully mapped address %s to %s', address.address, forwardAddr);
 
+    // Next, we will parse the file we were given by Receiver. The file should
+    // be an SMTP message body that Receiver wrote to the file system. To begin
+    // the process of reading, we instantiate a new parser.
     var parser = new MailParser({ streamAttachments: true });
+    // Once we have our new parser, we define what we should do once parsing
+    // is done. In this case, we are buffering the entire email body into
+    // memory, moving forward we may be able to optimize this out using streams
+    // if this application ends up receiving quite a bit of traffic.
     parser.on('end', function parsedSMTP (email) {
+      // Ensure the parsed message had all of the bits we expected, if not then
+      // the message is corrupt and there isn't really anything we can do
+      // (especially since we can't trust the message at this point)
+      if(!email.from || !email.subject || !email.text || !email.html) {
+        return cb(new Error('Invalid SMTP message'))
+      }
+
+      /*
+       * TODO: Detect if email is for registration, if not forward it on.
+      // Next, we coerce the incomming email message into a format mailer
+      // understands
       var opts = {
         to: forwardAddr,
         from: email.from,
@@ -27,18 +67,12 @@ function onEmail(address, pathname, cb) {
         html: email.html
       };
 
-      if(!opts.from || !opts.subject || !opts.text || !opts.html) {
-        return cb(new Error('Invalid SMTP message'))
-      }
-
-      /*
-       * TODO: Detect if email is for registration, if not forward it on.
       log.info('Forwarding email to address %s', forwardAddr);
 
       return mailer._transporter.sendMail(opts, function mailSent(e, info) {
         if(e) {
           return log.error('Error sending email for %s: %s', forwardAddr, e)
-        }
+          }
 
         log.info('Email for %s sent with messageId %s', forwardAddr, info.messageId);
         return cb(e);
@@ -46,17 +80,26 @@ function onEmail(address, pathname, cb) {
       */
 
       log.info(`Auto accepting registration for ${forwardAddr}`);
+      // Once we have loaded the email from disk, and have confirmed that this
+      // message was a registration email, we can auto accept registration on
+      // behalf of the user.
       autoaccept(forwardAddr, email.from, email.html, function accepted(e) {
+        // The callback simply lets us log a message before calling the cb
         if(e) {
           log.error(`Failed to auto accept: ${e.message}`);
         }
         return cb(e);
       });
     });
+    // Now that we have a handler registered, go ahead and start piping the
+    // SMTP message into the parser, once it is done loading off of disk it
+    // will trigger the above logic.
     fs.createReadStream(pathname).pipe(parser);
   });
 }
 
+// If Receiver encounters an error, this is the error handling logic it should
+// use instead of throwing.
 function onError (e) {
   if(e) return log.error(e);
 }
@@ -65,17 +108,22 @@ function onError (e) {
 // the command line, this function is called to startup the app. When required
 // in by the tests, this is the function exported.
 function bootstrap (cb) {
+  // Attach all of our instantiated objects to a single object for easy
+  // tracking. This lets our unit tests interact with the individual
+  // components instantiated by this file.
   var result = {}
+  // Create a new receiver, and register our onEmail logic above to it.
   new Receiver(
     {
       port: config.receiver.port,
       host: config.receiver.host,
       tmpdir: config.receiver.tmpdir,
+      // the `this` context will contain the heroku and mailer objects for the
+      // onEmail and onError functions.
       onEmail: onEmail.bind(result),
       onError: onError.bind(result)
     },
     function SMTPUp (e, smtp) {
-      // TODO: rollback heroku and mailer if needed
       result.receiver = smtp;
       return cb(e, result);
   });
